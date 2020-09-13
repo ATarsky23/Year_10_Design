b07745ae1fdb87bc47088d791f0c60fce772520a
Adam Tarsky <adam.tarsky@GCCYJQ2BJ1WK.lan>
2020-09-13 18:54:36 -0400

    2 tools

---
+++ Tools/toolsAT.py
@@ -0,0 +1,24 @@
+'''
+IsEven takes a single interger paramater a >= 0 returning true if a is even
+and false if a is odd.
+'''
+def IsEven(a):
+
+	if a % 2 == 0:
+	 return True
+
+	return False
+
+'''
+Test Code
+'''
+for i in range(0, 100, 1):
+	print(IsEven(i))
+
+
+def missing_char(str, n):
+  
+  
+ newStr = ""
+ newStr = str[0:n] + str[n + 1: len(str)]
+ return newStr
